0e39b91015eee862ef2d3f30e9b053213725d95a
ABHINAV RAI <allenrai1530@gmail.com>
2024-02-04 23:12:18 +0530

    Update firbase.js

--- src/firbase.js
+++ src/firbase.js
@@ -1,13 +1,7 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDm5w8V48pMPWne0ZSIjjdafQkctuV5_uQ",
-    authDomain: "linkedin-clone-475dd.firebaseapp.com",
-    projectId: "linkedin-clone-475dd",
-    storageBucket: "linkedin-clone-475dd.appspot.com",
-    messagingSenderId: "951391649348",
-    appId: "1:951391649348:web:918a8812cdbde3ca330611",
-    measurementId: "G-SX9KPTCRD4"
+   // API KEYS
   };
  
  const firebaseApp= firebase.initializeApp(firebaseConfig);
